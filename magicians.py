aab300dbeed5e217a5490f8d5bf68466ae0fa8df
iammj7 <jagddish13@gmail.com>
2021-10-28 17:29:21 +0530

    Chapter 4

---
+++ magicians.py
@@ -0,0 +1,11 @@
+# looping through an entire list
+magicians = ['alice', 'david', 'carolina',]
+for magician in magicians: # magician is a temparary var that exits withn that loop
+    #print(magician)
+    # let do some more work within this loop 
+    #print(f"{magician.title()}, that was great trick!")
+    print(f"I can't wait to see your next trick, {magician.title()}.\n")
+
+# do something after a for loop
+print("Thank you, everyone. That was a great magic show!")
+
