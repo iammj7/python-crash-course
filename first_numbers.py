67fabea5d66be0893b689e2e5d968dd22ba13d51
iammj7 <jagddish13@gmail.com>
2021-10-28 17:34:24 +0530

    Chapter 4

---
+++ first_numbers.py
@@ -0,0 +1,9 @@
+for value in range(1, 6): # range function can print 1,4 excluding last number
+    print(value)
+
+
+# make a list of numbers using range()
+numbers = list(range(1, 6)) # list is data type
+print(numbers)
+
+
