5863109e5099f17d2b62c4cc5a8baf35066e071d
iammj7 <jagddish13@gmail.com>
2021-10-28 17:12:44 +0530

    remove

--- 3.py
+++
@@ -1,8 +0,0 @@
-# using variable in strings
-first_name = "ada"
-last_name = "lovelace"
-# we used f format for space in between
-full_name = f"{first_name} {last_name}"
-print(full_name)
-print(f'hello, {full_name.title()}!')
-full_name = "{} {}".format(first_name, last_name)
--- 4.py
+++
@@ -1,7 +0,0 @@
-# striping white space
-#  rstrip(), lstrip(), strip()
-favourite_language = " python "
-print(favourite_language)
-print(favourite_language.rstrip())
-print(favourite_language.lstrip())
-print(favourite_language.strip())
